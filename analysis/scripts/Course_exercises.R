8056df55b7da11126ffeab7db7006614f81cb9be
Ines Hidalgo <ines.hidalgo@cos.uni-heidelberg.de>
2024-06-06 14:01:09 +0200

    Added some comments

--- analysis/scripts/Course_exercises.R
+++ analysis/scripts/Course_exercises.R
@@ -7,7 +7,7 @@
 install.packages("tidyverse")
 
 # source multiple packages from one file
-source("analysis/scripts/packages_and_functions.R")
+source("analysis/scripts/packages_and_functions.R") #we are sourcing/loading all the packages we need during the analysis
 
 #check your working dir (should be the .rproject dir)
 getwd()
@@ -25,7 +25,7 @@ getwd()
 # all good again
 
 list.files()
-
+list.files("analysis/data")
 #loading packages
 library(tidyverse)
 library(png)
@@ -35,7 +35,7 @@ library(png)
 
 #save session info and Rstudio version info for reproducibility
 sessionInfo()
-writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
+writeLines(capture.output(sessionInfo()), "sessionInfo.txt") #captures the output in a file
 
 # load data ---------------------------------------------------------------
 
@@ -100,7 +100,7 @@ data_Ashwini_sel
 # Add mean and SD columns with group_by() and mutate() --------
 
 data_Ashwini_sel_M_SD <- data_Ashwini_sel %>%
-  group_by(gene) %>%
+  group_by(gene, days) %>%
   mutate(mean2dct = mean(x2_dct)) %>%
   mutate(sd2dct = sd(x2_dct))
 data_Ashwini_sel_M_SD
@@ -144,8 +144,13 @@ plot_syn +
 head(iris)
 iris %>%  
   ggplot(aes(x = Petal.Length, y = Sepal.Width, color = Species)) +
-  geom_boxplot(notch = TRUE) +
+  geom_boxplot(notch = TRUE) + #with the notch we already do a statistical test. If the notches do not overlap, they are statistically different
   theme_minimal()
+#also possible to write it like this:
+#ggplot(iris, aes(x = Petal.Length, y = Sepal.Width, color = Species)) +
+#  geom_boxplot(notch = TRUE) +
+#  theme_minimal()
+
 iris %>%
   ggplot(aes(x = Sepal.Width)) +
   geom_histogram()
@@ -156,7 +161,7 @@ iris %>%
     color = Species, size = Sepal.Length)
     ) +
   geom_point() +
-  facet_wrap(~Species)
+  facet_wrap(~Species) #separate it into different plots for each species. If we leave this out, we plot all species in a single plot
 
 
 # Plot data - Jose ---------------
@@ -167,8 +172,8 @@ plot_Jose1 <- data_Jose %>%
   theme_minimal() +
   scale_fill_manual(values = c("#D55E00", "#E69F00", "#cccccc")) +
   guides(fill = guide_legend(title = "Treatment")) +
-  coord_flip() +
-  scale_y_log10()
+  coord_flip() + #flips the plot from vertical to horizontal
+  scale_y_log10() #to have log10 scale in the y axis
 
 plot_Jose1  
 
@@ -179,7 +184,7 @@ plot_Jose2 <- data_Jose %>%
   scale_fill_manual(values = c("#D55E00", "#E69F00", "#aaaaaa", "#dddddd")) +
   guides(fill = guide_legend(title = "Treatment")) +
   coord_flip() +
-  scale_y_log10()
+  scale_y_log10() 
 plot_Jose2
 
 
@@ -211,7 +216,7 @@ plot_syn <- tb_syn %>%
   theme_minimal()
 plot_syn
 
-# Read and preview data 3 --------------
+# Read and preview data 3 -------------- #This data is super messy
 
 data_Anchel <- rio::import("analysis/data/240323 CIN Exp278 reporter assay - Anchel.xlsx")
 head(data_Anchel)
@@ -222,11 +227,12 @@ summary(data_Anchel)
 # Save tidy data as source data for the plot/figure/paper -----------
 
 write_csv2(data_Ashwini_sel_M_SD, "manuscript/source_data/FigureX_Ashwini_source_data1.csv")
+#now if we go inside the project to manuscrip --> source data, you can find the file. You should do this for every plot. Good journals ask for the source data associated to a plot
 
 # check
 read_csv2("manuscript/source_data/FigureX_Ashwini_source_data1.csv")
 
-# Format plots with predefined complete ggplot2 themes ------------
+# Format plots with predefined complete ggplot2 themes ------------ #Good to use the same theme for all plots in your publication 
 plot_Jose1
 plot_Jose1 +
   theme_dark()
@@ -245,9 +251,9 @@ plot_Jose2 +
 
 # Format plots with a common custom theme() -------------
 
-args(theme)
+args(theme) #list of all things that you can modify in your plot (e.g. font, spacing, size of letters...); everything about the graphical/aesthetic aspect
 
-theme_plots <- theme_minimal() +
+theme_plots <- theme_minimal() + #we can modify the theme_mininmal() with + theme(different arguments of things we want to modify). We can save all the modifications/settings we like under the variable theme_plots. Then, we can add + theme_plots to all our plots so that they are all the same (below there are some examples)
   theme(
     axis.title.x = element_text(size = 14),
     axis.title.y = element_text(size = 12),
@@ -268,6 +274,11 @@ plot_Jose1 <- plot_Jose1 +
   theme_plots
 plot_Jose1
 
+plot_Jose1 <- plot_Jose1 +
+  theme_plots +
+  axis.text.x(element.text(angle=90))
+plot_Jose1
+
 plot_Jose2 <- plot_Jose2 +
   theme_plots
 plot_Jose2
@@ -276,19 +287,19 @@ plot_syn <- plot_syn +
   theme_plots
 plot_syn
 
-# Optional - save plots as png---------------
+# Optional - save plots as png--------------- #png have a fixed size. Better to use ggplot directly to assemble the different plots into a figure
 
 ggsave( "analysis/pictures/plot_Jose1a.png",
   limitsize = FALSE,
   units = c("px"), plot_Jose1,
-  width = 3400, height = 1600
+  width = 3400, height = 1600 #background will be transparent
   )
 
 # save in a different size
 ggsave( "analysis/pictures/plot_Jose1b.png",
   limitsize = FALSE,
   units = c("px"), plot_Jose2,
-  width = 2400, height = 2000, bg = "white"
+  width = 2400, height = 2000, bg = "white" # to have the background white
   )
 
 ggsave(
@@ -304,26 +315,56 @@ img1 <- readPNG("analysis/pictures/plot_Jose1a.png")
 img2 <- readPNG("analysis/pictures/plot_Jose1b.png")
 
 #convert to panels
-panel_JoseA <- ggdraw() + draw_image(img1)
+panel_JoseA <- ggdraw() + draw_image(img1) #converts image into a panel
 panel_JoseB <- ggdraw() + draw_image(img2)
 
-#define layout with textual representation
+#define layout with textual representation #define the layout for the figure
 layout <- "
 AB
 CD"
+#other option would be
+layout <- "
+ABCD"
+#other options are
+layout <- "
+ABCDEF
+GHHHHI
+JHHHHK
+LHHHHM
+NOPQRS"
+layout <- "
+ABCDEF
+AHHHHI
+JHHHHK
+LHHHHM
+NOPQRS"
+#you can define empty space with #
+#if you run out of alphabetic letters, you can use capital and small letters
+#check cheatsheet/tutorial of patchwork
 
 #assemble multipanel figure based on layout
 Figure_Jose <- panel_JoseA + panel_JoseB + plot_Jose1 + plot_Jose2 +
-  plot_layout(design = layout, heights = c(1, 1)) +
+  plot_layout(design = layout, heights = c(1, 1)) + #heights of the pannels
+  plot_annotation(tag_levels = 'A') & 
+  theme(plot.tag = element_text(size = 12, face='plain')) #tags of the panels
+#you can also add insets to your figure 
+
+layout <- "
+AB
+##
+CD"
+Figure_Jose <- panel_JoseA + panel_JoseB + plot_Jose1 + plot_Jose2 +
+  plot_layout(design = layout, heights = c(1, 0.05, 1)) + #heights of the panels
   plot_annotation(tag_levels = 'A') & 
   theme(plot.tag = element_text(size = 12, face='plain'))
+
 
 #save figure as png and pdf
 ggsave(
   "manuscript/figures/Figure_Jose.png", limitsize = FALSE, 
   units = c("px"), Figure_Jose, width = 4000, height = 1600,
   bg = "white"
-  )
+  ) #you cantry to save the image with different dimensions. This is quite critical for your final figure
 
 ggsave(
   "manuscript/figures/Figure_Jose.pdf", limitsize = FALSE, 
@@ -332,6 +373,16 @@ ggsave(
 
 image_read("manuscript/figures/Figure_Jose.png")
 
+#it is also possible to instead of first saving the plots as png and then read them, use the variable you saved the plot to to make the panels directly
+#because although we used the same theme for all the plots, when we saved them as png we saved them with different dimensions, and then it is very difficult to adjust the exact same font size for all plots 
+#we can also save all plots with the same theme and same dimensions?
+Figure_Jose <- plot_Jose1 + plot_Jose2 +
+  plot_layout(design = layout, heights = c(1, 1)) +
+  plot_annotation(tag_levels = 'A') & 
+  theme(plot.tag = element_text(size = 12, face='plain'))
+
+
+
 # Annotating a ggplot object ----------------
 
 plot_syn_ann <- plot_syn +
@@ -339,10 +390,13 @@ plot_syn_ann <- plot_syn +
   annotate("text", x = 34, y = 300, label = "30 sec", size = 3)
 plot_syn_ann
 
+plot_Jose1 +
+  annotate("segment", x = 1, xend = 2, y = 1, linewidth = 1)
 
 # Annotating an image ------------
 
 #read images
+#first you do some image processing in imageJ and the final image we import here
 img_INNOS <- magick::image_read("analysis/pictures/INNOS_synapses.png")
 
 #define arrow endpoints 
@@ -394,18 +448,21 @@ image_read("manuscript/figures/Figure_IHC.png")
 
 #read images and make annotated panel
 panel_NOS2d_HCR <- ggdraw() + draw_image(readPNG("analysis/pictures/HCR-IHC_51_AP_NOS_actub_56um.png")) +
-  draw_label("in situ HCR", x = 0.3, y = 0.99, size = 10) +
-  draw_label("NOS", x = 0.12, y = 0.9, color="magenta", size = 11, fontface="italic") +
-  draw_label("acTub", x = 0.36, y = 0.9, color="green", size = 11, fontface="plain") +
-  draw_line(x = c(0.1, 0.46), y = c(0.08, 0.08), color = "white", size = 0.5) +
-  draw_label(expression(paste("20 ", mu, "m")), x = 0.28, y = 0.11, color = "white", size = 8)
-  
+  draw_label("in situ HCR", x = 0.3, y = 0.99, size = 10) + #title of the panel
+  draw_label("NOS", x = 0.12, y = 0.9, color="magenta", size = 11, fontface="italic") + #this adds a label
+  draw_label("acTub", x = 0.36, y = 0.9, color="green", size = 11, fontface="plain") + #this adds another label
+  draw_line(x = c(0.1, 0.46), y = c(0.08, 0.08), color = "white", size = 0.5) + #this makes the scalebar
+  draw_label(expression(paste("20 ", mu, "m")), x = 0.28, y = 0.11, color = "white", size = 8) #label of the scalebar
+  #expression and mu is to get the micro letter (greek) 
 panel_NIT_HCR <- ggdraw() + draw_image(readPNG("analysis/pictures/HCR_72_AP_NIT_94um.png")) +
   draw_label("transgene + IHC", x = 0.5, y = 0.99, size = 10) +
   draw_label("NOSp::palmi-3xHA", x = 0.34, y = 0.9, color="magenta", size = 10, fontface="plain") +
   draw_label("acTub", x = 0.8, y = 0.9, color="green", size = 10, fontface="plain") +
   draw_line(x = c(0.1, 0.31), y = c(0.08, 0.08), color = "white", size = 0.5) 
 panel_NIT_HCR
+
+#magick package allows to work with TIFs instead of pngs
+?magick
 
 # introduce gaps in layout --------------
 
@@ -421,7 +478,7 @@ Figure_scalebars <- panel_NOS2d_HCR + panel_NIT_HCR +
 ggsave(
   "manuscript/figures/Figure_scalebars.png",
   units = c("px"), Figure_scalebars, 
-  width = 1700, height = 940, bg = "white"
+  width = 1700, height = 940, bg = "white" #the size of the figure is the width of the two confocal images (800 pixel each) plus a little gap and the height of the two confocal images (800 pixel each) plus the text
   )
 
 ggsave(
@@ -430,14 +487,52 @@ ggsave(
   width = 1700, height = 940
   )
 image_read("manuscript/figures/Figure_scalebars.png")
+image_read("manuscript/figures/Figure_scalebars.pdf")
 
 # Fine-tuning figure size and gaps ----------------
 
 # Excercises
 # - save the figure in different sizes 
+ggsave(
+  "manuscript/figures/Figure_scalebars.png",
+  units = c("px"), Figure_scalebars, 
+  width = 2000, height = 2000
+)
 # - introduce gap with # into layout, also need to define width of gap as say 0.05
-# - change position of scalebar and scalebar legend
+layout <- "A#B"
+Figure_scalebars <- panel_NOS2d_HCR + panel_NIT_HCR +
+  plot_layout(design = layout, widths = c(1, 0.05, 1)) +
+  plot_annotation(tag_levels = 'A') & 
+  theme(plot.tag = element_text(size = 12, face='plain'))
 
+ggsave(
+  "manuscript/figures/Figure_scalebars.png",
+  units = c("px"), Figure_scalebars, 
+  width = 1700, height = 940
+)
+# - change position of scalebar and scalebar legend
+panel_NOS2d_HCR <- ggdraw() + draw_image(readPNG("analysis/pictures/HCR-IHC_51_AP_NOS_actub_56um.png")) +
+  draw_label("in situ HCR", x = 0.3, y = 0.99, size = 10) + 
+  draw_label("NOS", x = 0.12, y = 0.9, color="magenta", size = 11, fontface="italic") + #this adds a label
+  draw_label("acTub", x = 0.36, y = 0.9, color="green", size = 11, fontface="plain") + #this adds another label
+  draw_line(x = c(0.1, 0.46), y = c(1, 0.08), color = "white", size = 0.5) + #this makes the scalebar
+  draw_label(expression(paste("20 ", mu, "m")), x = 0.1, y = 0.5, color = "white", size = 8) #label of the scalebar
+
+panel_NIT_HCR <- ggdraw() + draw_image(readPNG("analysis/pictures/HCR_72_AP_NIT_94um.png")) +
+  draw_label("transgene + IHC", x = 0.5, y = 0.99, size = 10) +
+  draw_label("NOSp::palmi-3xHA", x = 0.34, y = 0.9, color="magenta", size = 10, fontface="plain") +
+  draw_label("acTub", x = 0.8, y = 0.9, color="green", size = 10, fontface="plain") +
+  draw_line(x = c(0.1, 0.31), y = c(0.5, 0.5), color = "white", size = 0.5) 
+
+Figure_scalebars <- panel_NOS2d_HCR + panel_NIT_HCR +
+  plot_layout(design = layout, widths = c(1, 0.05, 1)) +
+  plot_annotation(tag_levels = 'A') & 
+  theme(plot.tag = element_text(size = 12, face='plain'))
+ggsave(
+  "manuscript/figures/Figure_scalebars.png",
+  units = c("px"), Figure_scalebars, 
+  width = 1700, height = 940
+)
 #no gap in layout
 layout1 <- "AB"
 
@@ -526,6 +621,13 @@ ggsave(
   units = c("cm"), Figure_complex, 
   width = 22, height = 13, dpi = 300, bg = "white"
   )
+image_read("manuscript/figures/Figure_complex_300dpi.png")
+
+
+# Read tif files -------------
+install.packages("magick") #also allows to read svg files
+?magick
+?image_read()
 
 # Statistical comparisons -------------
 
@@ -538,3 +640,5 @@ data_Jose %>%
   guides(fill = guide_legend(title = "Treatment")) +
   coord_flip() +
   scale_y_log10()
+
+
